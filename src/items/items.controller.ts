da29c10fd6ef94cb07ff6d22138493e4855d5df1
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2022-05-03 23:48:28 +0900

    add: uuidインストール・設定

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { Item } from './item.model';
 import { ItemsService } from './items.service';
@@ -15,7 +15,8 @@ export class ItemsController {
 
   @Get(':id') //コロン(:)を前につけることで可変のパラメーターとして定義できる
   //パラメーター取得の為@Paramデコレーターを使用
-  findById(@Param('id') id: string): Item {
+  //parseuuidパイプを渡す
+  findById(@Param('id', ParseUUIDPipe) id: string): Item {
     //ServiceのメソッドにIDを渡す
     return this.itemsService.findById(id);
   }
@@ -23,20 +24,20 @@ export class ItemsController {
   //createメソッドを定義
   @Post()
   //DTOを変数で定義
-  create(@Body() CreateItemDto: CreateItemDto): Item {
+  create(@Body() createItemDto: CreateItemDto): Item {
     //ServiceクラスのCreateメソッドを呼び出す
     //createItemDtoをServiceにわたす
     return this.itemsService.create(createItemDto);
   }
   //updateなのでpatchを利用
   @Patch(':id')
-  updateStatus(@Param('id') id: string): Item {
+  updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.itemsService.updateStatus(id);
   }
 
   //deleteメソッド
   @Delete(':id')
-  delete(@Param('id') id: string): void {
+  delete(@Param('id', ParseUUIDPipe) id: string): void {
     //取得したidをServiceに渡す
     this.itemsService.delete(id);
   }
