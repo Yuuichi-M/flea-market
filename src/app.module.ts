faa99d7da2c37bfa93c9652b0fa17185f37a62d5
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2022-05-12 21:40:18 +0900

    add: TypeORMのインストール、Moduleの設定に追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; //typeOrmModuleをインポート
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    //TypeOrmの環境設定
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
