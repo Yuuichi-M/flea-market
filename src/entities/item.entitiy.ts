2c1a210a2a067bdfcc42b0c16f9d406f67d8d099
Yuuichi-M <yuu555.dive.no.1@gmail.com>
2022-05-12 22:09:08 +0900

    add: Entitiyの作成

---
+++ src/entities/item.entitiy.ts
@@ -0,0 +1,30 @@
+import { ItemStatus } from "src/items/item-status.enum";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+//このEntitiyを利用してデータベースにテーブルを作成する準備
+//entitiyとして動作させる
+@Entity()
+export class Item {
+  //自動採番カラムとして表す
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  //通常のカラム
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: ItemStatus;
+
+  @Column()
+  createdAt: string;
+
+  @Column()
+  updatedAt: string;
+}
